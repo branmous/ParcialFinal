6c1a55075286441191e5ea7fae805dfd44379418
BRANDON STEVEN MONTOYA USUGA <brandonmontoya191272@correo.itm.edu.co>
2018-11-23 07:41:06 -0500

    Queries function 1

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION CALCULAR_CAJAS_NECESARIAS(num_Item IN NUMBER,cajas_grandes IN NUMBER,cajas_pequnas IN NUMBER) 
    RETURN NUMBER 
-   IS despachar NUMBER(11,2);
+   IS despachar NUMBER;
    valueroundCG number;
    totalgrandes number;
    faltante number;
    BEGIN 
-   valueroundCG := ROUND(num_Item/5) -1;
+   valueroundCG := ROUND(num_Item/5);
    if valueroundCG <= cajas_grandes then
     totalgrandes := 5*valueroundCG;
     faltante := num_Item-totalgrandes;
@@ -14,13 +14,13 @@ CREATE OR REPLACE FUNCTION CALCULAR_CAJAS_NECESARIAS(num_Item IN NUMBER,cajas_gr
    end if;
    
    if faltante = 0 then
-     despachar := 1;
+     despachar := totalgrandes;
    end if;
-   if faltante > cajas_pequnas then
+   if faltante > cajas_pequnas or faltante < 0 then
     despachar := -1;   
    end if;
-   if faltante <= cajas_pequnas then
-    despachar := 1;   
+   if faltante <= cajas_pequnas AND faltante > 0 then
+    despachar := valueroundCG + faltante;       
    end if;      
       RETURN(despachar); 
     END;
